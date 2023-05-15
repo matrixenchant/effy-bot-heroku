02f84269c05bae2a73922d6a24d7c443e31f9747
Maksim Kuznetsov <m_kuznetsov@kbtu.kz>
2023-05-15 23:53:44 +0600

    init

---
+++ bot.py
@@ -0,0 +1,21 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(token=TOKEN)
+
+# Bot's Functionalities
+def sendMessage(message, text):
+   bot.send_message(message.chat.id, text)
+# This method will send a message formatted in HTML to the user whenever it starts the bot with the /start command, feel free to add as many commands' handlers as you want
+@bot.message_handler(commands=['start'])
+def send_info(message):
+   text = (
+   "<b>Welcome to the Medium 🤖!</b>\n"
+   "Say Hello to the bot to get a reply from it!"
+   )
+   bot.send_message(message.chat.id, text, parse_mode='HTML')
+# This method will fire whenever the bot receives a message from a user, it will check that there is actually a not empty string in it and, in this case, it will check if there is the 'hello' word in it, if so it will reply with the message we defined
+@bot.message_handler(func=lambda msg: msg.text is not None)
+def reply_to_message(message):
+   if 'hello'in message.text.lower():
+      sendMessage(message, 'Hello! How are you doing today?')
---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+import telebot
+from bot import bot
+from config import TOKEN
+import os
+
+server = Flask(__name__)
+
+# SERVER SIDE
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+   return "!", 200
+
+@server.route("/")
+def webhook():
+   bot.remove_webhook()
+   bot.set_webhook(url='https://effy-bot.herokuapp.com/' + TOKEN)
+   return "!", 200
